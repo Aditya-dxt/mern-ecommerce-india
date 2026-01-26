ee4ef56ece775969c45fa4cb494748ad7343a92c
Aditya-dxt <adityadxt1910@gmail.com>
2026-01-26 23:43:15 +0530

    Day-06: Implement Order model and order APIs

---
+++ backend/src/controllers/orderController.js
@@ -0,0 +1,46 @@
+import Order from "../models/Order.js";
+
+export const createOrder = async (req, res) => {
+  try {
+    const { items, address, paymentMethod } = req.body;
+
+    if (!items || !address) {
+      return res.status(400).json({ message: "Items & address required" });
+    }
+
+    const totalAmount = items.reduce(
+      (sum, item) => sum + item.price * item.quantity,
+      0
+    );
+
+    const order = await Order.create({
+      userId: req.user._id, // 🔥 from token
+      items,
+      address,
+      totalAmount,
+      paymentMethod,
+    });
+
+    res.status(201).json({ success: true, order });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+export const getAllOrders = async (req, res) => {
+  try {
+    const orders = await Order.find().sort({ createdAt: -1 });
+
+    res.status(200).json({
+      success: true,
+      count: orders.length,
+      orders,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
