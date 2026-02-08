51f3c8bc4489f91d0683fd4b6c7bb241c77e7b41
Aditya-dxt <adityadxt1910@gmail.com>
2026-02-08 23:11:06 +0530

    feat(day-11 frontend): stock-aware cart, checkout validation, UI updates

---
+++ frontend/src/utils/axios.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "http://localhost:5000/api",
+});
+
+// 🔐 Attach JWT automatically
+instance.interceptors.request.use(
+  (config) => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    const token = user?.token;
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default instance;
